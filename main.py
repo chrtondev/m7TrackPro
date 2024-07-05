71a3da04e0f7aca61b7873e5ed804d6df81d9e0d
Mr Mask <ldaiwa7@gmail.com>
2024-07-05 11:07:47 -0400

    added csv logging feature

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-# i am in main.py
+# main.py
 import asyncio
 from scanner import find_device_by_name
 from connector import HeartRateMonitor
 from workout_manager import WorkoutManager
+import os
 
-# this can be changed to target other heart rate monitors
 TARGET_DEVICE_NAME = "M7-08252"
 
 async def main():
-    monitor = HeartRateMonitor()
     manager = WorkoutManager()
+    monitor = HeartRateMonitor(manager)
     device = await find_device_by_name(TARGET_DEVICE_NAME)
     
     if device:
@@ -18,9 +18,19 @@ async def main():
         while True:
             action = input("Enter '1' to start workout, '2' to end workout, '3' to exit: ")
             if action == '1':
-                await manager.start_workout()
+                manager.start_workout()
             elif action == '2':
-                await manager.end_workout()
+                data, duration = manager.end_workout()
+                
+                # Generate filename
+                filename = f"workout_data/workout_{manager.start_time.strftime('%Y-%m-%d_%H%M%S')}.csv"
+                
+                # Ensure the workout_data directory exists
+                os.makedirs(os.path.dirname(filename), exist_ok=True)
+                
+                # Log data to CSV
+                manager.log_to_csv(filename, data)
+                
             elif action == '3':
                 if monitor.client:
                     await monitor.client.disconnect()
